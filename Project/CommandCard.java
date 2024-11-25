9d53b88686770acfc9686777f23bd496f8f23ef8
Amaury <amau.dufrenot@gmail.com>
2024-11-25 16:47:15 +0100

    ajout

---
+++ Project/CommandCard.java
@@ -0,0 +1,14 @@
+package Project;
+
+public class CommandCard {
+    private String id;
+    private String color;
+
+    public void executeCard(){};
+
+    public void invade(){}; //Pourquoi faire ?
+
+    public void expand(){};
+
+    public void explore(){};
+}
