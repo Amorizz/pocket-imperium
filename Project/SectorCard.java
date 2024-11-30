11e19fefaad2a51598e3468696a40051fbb06b8f
Estebanlooo <93783258+Estebanlooo@users.noreply.github.com>
2024-11-30 13:46:49 +0100

    Caca

--- Project/SectorCard.java
+++ Project/SectorCard.java
@@ -1,5 +1,6 @@
 package Project;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,7 +14,15 @@ public class SectorCard {
     private boolean top;
     private Map<Integer, Hex> Hexa;
 
-
+    public ArrayList<Integer> position21(int nbr){
+        ArrayList<Integer> LNBR = new ArrayList<>();
+        while(LNBR.size() != 3){
+            Random randomNumbers = new Random();
+            int n = randomNumbers.nextInt(nbr);
+            LNBR.add(n);
+        }
+        return LNBR;
+    }
 
     public SectorCard(int number, boolean triPrime, boolean bottom, boolean top) {
         this.number = number;
@@ -22,39 +31,29 @@ public class SectorCard {
         this.top = top;
         this.Hexa = new HashMap<Integer, Hex>();    // Les hexagones seront ajouté dans une map (dictionnaire)
         if (this.bottom || this.top) {              // Création des Hexagons dans la carte secteur si c'est une carte bottom ou top
-            Random randomNumbers = new Random();
-            Hex Hex1 = new Hex(randomNumbers.nextInt(3));
-            this.Hexa.put(1, Hex1);
-            Hex Hex2 = new Hex(randomNumbers.nextInt(3));
-            this.Hexa.put(2, Hex2);
-            Hex Hex3 = new Hex(0);
-            this.Hexa.put(3, Hex3);
-            Hex Hex4 = new Hex(randomNumbers.nextInt(3));
-            this.Hexa.put(4, Hex4);
-            Hex Hex5 = new Hex(0);
-            this.Hexa.put(5, Hex5);
-            Hex Hex6 = new Hex(randomNumbers.nextInt(3));
-            this.Hexa.put(6, Hex6);
-            Hex Hex7 = new Hex(randomNumbers.nextInt(3));
-            this.Hexa.put(7,Hex7);
+            ArrayList<Integer> LNBR = position21(7);
+            for (int i = 0;i<7;i++){
+                Hex Hex1 = new Hex(0);
+                this.Hexa.put(i+1, Hex1);
+            }
+            Hex Hex2 = new Hex(2);
+            this.Hexa.put(LNBR.getFirst(), Hex2);
+            Hex Hex11 = new Hex(1);
+            this.Hexa.put(LNBR.get(1), Hex11);
+            Hex Hex12 = new Hex(1);
+            this.Hexa.put(LNBR.get(2), Hex12);
         } else if (!this.bottom && !this.top) {     // Création des Hexagons dans la carte secteur si ce n'est pas une carte bottom ou top ou triPri
-            Random randomNumbers = new Random();
-            Hex Hex1 = new Hex(0);
-            this.Hexa.put(1, Hex1);
-            Hex Hex2 = new Hex(randomNumbers.nextInt(3));
-            this.Hexa.put(2, Hex2);
-            Hex Hex3 = new Hex(0);
-            this.Hexa.put(3,Hex3);
-            Hex Hex4 = new Hex(randomNumbers.nextInt(3));
-            this.Hexa.put(4, Hex4);
-            Hex Hex5 = new Hex(randomNumbers.nextInt(3));
-            this.Hexa.put(5, Hex5);
-            Hex Hex6 = new Hex(0);
-            this.Hexa.put(6, Hex6);
-            Hex Hex7 = new Hex(randomNumbers.nextInt(3));
-            this.Hexa.put(7, Hex7);
-            Hex Hex8 = new Hex(0);
-            this.Hexa.put(8, Hex8);
+            ArrayList<Integer> LNBR = position21(8);
+            for (int i = 0;i<8;i++){
+                Hex Hex1 = new Hex(0);
+                this.Hexa.put(i+1, Hex1);
+            }
+            Hex Hex2 = new Hex(2);
+            this.Hexa.put(LNBR.getFirst(), Hex2);
+            Hex Hex11 = new Hex(1);
+            this.Hexa.put(LNBR.get(1), Hex11);
+            Hex Hex12 = new Hex(1);
+            this.Hexa.put(LNBR.get(2), Hex12);
         } else {                                    // Création des Hexagons dans la carte secteur si c'est la carte triPri
             Random randomNumbers = new Random();
             Hex Hex1 = new Hex(0);
